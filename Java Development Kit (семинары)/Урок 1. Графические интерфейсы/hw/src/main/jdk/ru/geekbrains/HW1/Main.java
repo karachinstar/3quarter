41031668e7b6ea928bb9e1ccf8742d64f01d3077
esa <karachinstar@yandex.ru>
2023-12-19 17:41:39 +0800

    Переделал по примеру от преподавателя

--- Java Development Kit (семинары)/Урок 1. Графические интерфейсы/hw/src/main/jdk/ru/geekbrains/HW1/Main.java
+++ Java Development Kit (семинары)/Урок 1. Графические интерфейсы/hw/src/main/jdk/ru/geekbrains/HW1/Main.java
@@ -4,6 +4,7 @@ import ru.geekbrains.HW1.server.ServerWindow;
 
 public class Main {
     public static void main(String[] args) {
-        new ServerWindow();
+       new ServerWindow();
+
     }
 }
--- Java Development Kit (семинары)/Урок 1. Графические интерфейсы/hw/src/main/jdk/ru/geekbrains/HW1/server/ServerWindow.java
+++ Java Development Kit (семинары)/Урок 1. Графические интерфейсы/hw/src/main/jdk/ru/geekbrains/HW1/server/ServerWindow.java
@@ -5,29 +5,126 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerWindow extends JFrame {
     private static final int POS_X = 500;
     private static final int POS_Y = 550;
     private static final int WIDTH = 400;
     private static final int HEIGHT = 300;
+    public static String LOG_FILE = "chat_log.txt";
 
-    private final JButton btnStart = new JButton("Start");
-    private final JButton btnStop = new JButton("Stop");
-    private final JButton openUserChat = new JButton("Open chat");
-    public static final JTextArea logServer = new JTextArea();
-    public static boolean isServerWorking;
+
+    List<ClientWindowChat> clientWindowChatList;
+    JButton btnStart;
+    JButton btnStop;
+    JButton openUserChat;
+
+    JTextArea logServer;
+    boolean isServerWorking;
 
     public ServerWindow(){
-        isServerWorking = false;
+        clientWindowChatList = new ArrayList<>();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat server");
+        setLocationRelativeTo(null);
+        createPanel();
+        setVisible(true);
+    }
+
+    public boolean connectUser(ClientWindowChat clientWindowChat){
+        if(!isServerWorking){
+            return false;
+        }
+        clientWindowChatList.add(clientWindowChat);
+        return true;
+    }
+
+    public String getLog(){
+        return loadChatHistory();
+    }
+
+    public void disconnectUser(ClientWindowChat clientWindowChat){
+        clientWindowChatList.remove(clientWindowChat);
+        if (clientWindowChat != null){
+            clientWindowChat.disconnectFromServer();
+        }
+    }
+
+    public void message(String text){
+        if(!isServerWorking){
+            return;
+        }
+        text += "";
+        appendLog(text);
+        answerAll(text);
+        saveMessageToFile(text);
+    }
+
+    private void answerAll(String text){
+        for (ClientWindowChat clientWindowChat: clientWindowChatList){
+            clientWindowChat.answer(text);
+        }
+    }
+
+    private void saveMessageToFile(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            writer.write(message);
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadChatHistory() {
+        File file = new File(LOG_FILE);
+        StringBuilder stringBuilder = new StringBuilder();
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                return stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private void appendLog(String text){
+        logServer.append(text + "\n");
+    }
+
+    private void createPanel(){
+        logServer = new JTextArea();
+        logServer.setEditable(true);
+        add(new JScrollPane(logServer));
+        add(createButtons(), BorderLayout.SOUTH);
+        logServer.append(getLog());
+    }
+
+    private Component createButtons(){
+        JPanel panButton = new JPanel(new GridLayout(1, 3));
+        btnStart = new JButton("Start");
+        btnStop = new JButton("Stop");
+        openUserChat = new JButton("Open chat");
         btnStop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (isServerWorking) {
                     isServerWorking = false;
-                    logServer.append("Сервер остановлен \n");
+                    while (!clientWindowChatList.isEmpty()){
+                        disconnectUser(clientWindowChatList.get(clientWindowChatList.size() - 1));
+                    }
+                    appendLog("Сервер остановлен \n");
                 }else{
-                    logServer.append("Сервер уже остановлен \n");
+                    appendLog("Сервер уже остановлен \n");
                 }
             }
         });
@@ -37,33 +134,21 @@ public class ServerWindow extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 if (!isServerWorking) {
                     isServerWorking = true;
-                    logServer.append("Запуск сервера \n");
+                    appendLog("Запуск сервера \n");
                 }else{
-                    logServer.append("Сервер уже работает \n");
+                    appendLog("Сервер уже работает \n");
                 }
             }
         });
-
         openUserChat.addActionListener(e -> {
             if(isServerWorking)
-                new ClientWindowChat();
-            else logServer.append("Сервер неактивен\n");
+                new ClientWindowChat(this);
+            else appendLog("Сервер остановлен\n");
         });
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
-        setResizable(false);
-        setTitle("Chat server");
-        setAlwaysOnTop(true);
-        JPanel panButton = new JPanel(new GridLayout(1, 3));
-        //setLayout(new GridLayout(1, 2));
         panButton.add(btnStart);
         panButton.add(btnStop);
         panButton.add(openUserChat);
-        add(panButton, BorderLayout.SOUTH);
-        add(logServer);
+        return panButton;
 
-
-        setVisible(true);
     }
 }
