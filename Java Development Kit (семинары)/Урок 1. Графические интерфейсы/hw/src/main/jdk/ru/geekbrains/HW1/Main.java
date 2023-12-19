4b23e1ae47478e4f9d0ca653c89fe112d4fd21f8
karachinstar <121330851+karachinstar@users.noreply.github.com>
2023-12-19 17:36:46 +0800

    Delete Java Development Kit (семинары)/Урок 1. Графические интерфейсы/hw/src/main/jdk/ru/geekbrains/HW1 directory
    фывфыв

--- Java Development Kit (семинары)/Урок 1. Графические интерфейсы/hw/src/main/jdk/ru/geekbrains/HW1/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.HW1;
-
-import ru.geekbrains.HW1.server.ServerWindow;
-
-public class Main {
-    public static void main(String[] args) {
-        new ServerWindow();
-    }
-}
